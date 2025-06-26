7236694c5c931145f174a10859d90db7109480d7
Cilliers Swanepoel <chrisswa@outlook.com>
2025-06-26 13:20:55 +0200

    Landing page greeting message - Initial commit

--- villajullienne/src/components/page/HomeLanding.jsx
+++ villajullienne/src/components/page/HomeLanding.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import "/src/static/css/page/HomeLanding.css";
 
 
 const HomeLanding = () => (
     <>
+        <div className="welcome-message">
+            <h1>Welcome to Villa Jullienne</h1>
+            <p>Your serene escape in the heart of nature</p>
+        </div>
 
     </>
 );
