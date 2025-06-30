7e3d89481b6a70db27410cfd304c997df41a9d61
Cilliers Swanepoel <chrisswa@outlook.com>
2025-06-30 15:29:52 +0200

    Update HomeLanding.jsx

--- villajullienne/src/components/page/HomeLanding.jsx
+++ villajullienne/src/components/page/HomeLanding.jsx
@@ -10,14 +10,9 @@ const HomeLanding = () => (
                 alt="Villa Jullienne"
                 className="welcome-image"/>
             <div className="welcome-message">
-                <h1>WELCOME <br/> to <br/> VILLA JULLIENNE</h1>
+                <h1>WELCOME <br/> to <br/> Elyon Panorama Village</h1>
                 <p>A HOME AWAY FROM HOME</p>
             </div>
-        </div>
-
-        <div className="carousel-container">
-
-
         </div>
 
     </>
