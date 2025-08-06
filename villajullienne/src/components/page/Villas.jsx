930f202b7b49b3473b49d96367ab0abd4f9ff0d7
Cilliers Swanepoel <chrisswa@outlook.com>
2025-08-06 14:52:14 +0200

    Updates Villas
    Partially incomplete

--- villajullienne/src/components/page/Villas.jsx
+++ villajullienne/src/components/page/Villas.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import '../../static/css/page/Villas.css';
+import TopView from '../../static/assets/page/homelanding/top_view.jpg';
 
 function Villas() {
     return (
         <div style={{ marginTop: 80 }}>
-            <h1>Villas</h1>
+            <div className='villas-welcome-message-container' style={{
+                backgroundImage: `url(${TopView})`,
+                position: 'relative',
+                width: '100vw',
+                height: '90vh',
+                margin: 0,
+                padding: 0,
+                overflow: 'hidden',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat'
+            }}>
+                <h1 className='villas-welcome-message-title'>Our Units @ Villa Jullienne</h1>
+                <h2 className='villas-welcome-message-description'></h2>
+            </div>
         </div>
     );
 }
