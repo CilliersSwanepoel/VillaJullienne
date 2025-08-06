01f250ce9eb2500c2da2ddfdbec9b723bfeea86f
Cilliers Swanepoel <chrisswa@outlook.com>
2025-08-06 11:52:47 +0200

    Removed Large Carousel
    Redesign of the Villas Page in effect

--- villajullienne/src/components/page/Villas.jsx
+++ villajullienne/src/components/page/Villas.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import DarkVariantExample from '../elements/CarouselLarge.jsx';
 
 function Villas() {
     return (
         <div style={{ marginTop: 80 }}>
             <h1>Villas</h1>
-            <DarkVariantExample />
         </div>
     );
 }
